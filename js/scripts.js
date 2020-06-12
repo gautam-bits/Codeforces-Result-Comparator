f0f5a9caabd0d5cfb34fd0fa13c10156af9d5049
hp <gautammps123456789@gmail.com>
2020-06-12 10:56:41 +0530

    Initial COmmit

---
+++ js/scripts.js
@@ -0,0 +1,88 @@
+
+function fetchdata(val1 , val2) {
+    //searchbutton.innerHTML = "jdbvdcxk";
+    //console.log("fddf");
+    let endpoint1 = `https://codeforces.com/api/user.rating?handle=${val1}`;
+    let endpoint2 = `https://codeforces.com/api/user.rating?handle=${val2}`;
+    //console.log(420);
+    let apiRequest1 = fetch(endpoint1).then(function(response){ 
+      return response.json()
+    });
+
+    let apiRequest2 = fetch(endpoint2).then(function(response){
+      return response.json()
+    });
+
+    Promise.all([apiRequest1,apiRequest2]).then(function(values){
+      //console.log(4743);
+      DisplayResults(values[0],values[1])  
+    });
+
+}
+
+function DisplayResults(you , his ) {
+
+    let array1 = you.result;
+    let array2 = his.result;
+
+    //console.log(array1);
+    array1.reverse();
+    //console.log(array1);
+
+    const html = array1.map(e => {
+      let ContestId = e.contestId;
+      let ContestName = e.contestName;
+      let YourRank = e.rank;
+      let YourRating = e.newRating;
+      let HisElement = array2.find(element => element.contestId == e.contestId) ;
+      let HisRank = -1;
+      let HisRating = -1;
+      let link = "https://codeforces.com/contest/"
+      let contestlink = "<a href = '" + link + ContestId.toString() + " ' target=\"_blank\">";
+      console.log(contestlink);
+      if(HisElement)
+      {
+        HisRank = HisElement.rank;
+        HisRating = HisElement.newRating;
+      }
+
+      return `
+      <tr>
+        <td class="column1">${contestlink}${ContestId}</a></td>
+        <td class="column2">${ContestName}</td>
+        <td class="column3">${YourRank}</td>
+        <td class="column4">${HisRank}</td>
+        <td class="column5">${YourRating}</td>
+        <td class="column6">${HisRating}</td>
+      </tr>
+      `;
+    }).join('');
+
+    tablebody.innerHTML = html;
+
+}
+
+
+
+
+
+let yourhandle = document.getElementById("your-handle");
+let hishandle = document.getElementById("his-handle");
+let searchbutton = document.getElementById("search-button");
+let tablebody = document.getElementById("table-body");
+//searchbutton.innerHTML = "dmjvv";
+//console.log("clickcheck");
+//let link = "https://codeforces.com/contest/"
+//let rand = "1331"
+//let contestlink = "<a href = '" + link + rand +   "'>";
+//console.log(contestlink);
+
+searchbutton.addEventListener("click" , function(){
+//alert("lolo");
+//console.log("fskjd");
+let yourusername = yourhandle.value;
+let hisusername = hishandle.value;
+fetchdata(yourusername, hisusername);
+
+});
+ 
